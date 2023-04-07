ef65a02f7f932a3cfafafab59b33fccc8b13bfaa
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-04-06 20:11:01 -0700

    second commit for define banner of website and style it and make object store of brands

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import DrawerAppBar from "./components/navbar/NavBar";
+import BannerFunction from "./components/banner/Banner";
+
+function App() {
+  return (
+    <div>
+      <DrawerAppBar>
+        <BannerFunction/>
+      </DrawerAppBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/banner/Banner.js
@@ -0,0 +1,13 @@
+import bannerImage from "../../assets/baner/how-to-design-the-perfect-website.jpg";
+import classes from "./Banner.module.css"
+
+function BannerFunction() {
+    return(
+        <div>
+            <img className={classes.container} src={bannerImage} alt="wellcome banner"/>
+        </div>
+    );
+}
+
+
+export default BannerFunction;
