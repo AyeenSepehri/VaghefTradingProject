026efe7ccff8b7f82f59b06717bc8bd2110d0dbc
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-04-08 20:03:51 +0330

    add cards section for brands in landing page

---
+++ src/components/cards/Card.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+
+export default function MultiActionAreaCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345 , my: 2 , display: "inline-block" , mx: 3}}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image= {props.image}
+          alt=""
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.brand}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.detail}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
