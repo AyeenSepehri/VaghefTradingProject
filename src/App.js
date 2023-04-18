a2c4c901244d145caed048485acd2f9066ad8dfc
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-04-18 20:23:10 +0330

    test on redux

--- src/App.js
+++ src/App.js
@@ -60,7 +60,6 @@ function App() {
         <BannerFunction/>
         {/* <Card/> */}
         {CardsOfBrands}
-        
       </DrawerAppBar>
     </div>
   );
--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,7 +1,10 @@
 import bannerImage from "../../assets/baner/how-to-design-the-perfect-website.jpg";
 import classes from "./Banner.module.css"
+import { useSelector } from "react-redux";
 
 function BannerFunction() {
+    const test = useSelector((state) => state.brandsData.data)
+    console.log(test)
     return(
         <div>
             <img className={classes.container} src={bannerImage} alt="wellcome banner"/>
