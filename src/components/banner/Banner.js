7343a5dcc82b0ec364a5e05607a4efc1852b7f79
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-04-19 16:36:26 +0330

    delete testing line of redux in this component

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,10 +1,8 @@
 import bannerImage from "../../assets/baner/how-to-design-the-perfect-website.jpg";
 import classes from "./Banner.module.css"
-import { useSelector } from "react-redux";
 
 function BannerFunction() {
-    const test = useSelector((state) => state.brandsData.data)
-    console.log(test)
+
     return(
         <div>
             <img className={classes.container} src={bannerImage} alt="wellcome banner"/>
