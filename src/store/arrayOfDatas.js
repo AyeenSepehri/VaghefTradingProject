f4d80a8cae8fa75ac707969c5968ac4eb5e50cb2
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-04-19 16:46:08 +0330

    create a js file for store array of object of brands data

---
+++ src/store/arrayOfDatas.js
@@ -0,0 +1,93 @@
+import staggLogo from "../assets/Stagg Images/staggLogo.jpg";
+import steveLogo from "../assets/Steve Images/esteve-logo.jpg";
+import steinwayLogo from "../assets/Steinway Images/steinway-and-sons-logo.png";
+import rotosoundLogo from "../assets/Rotosound Images/Header-Rotosound-Logo.png";
+import youngChangLogo from "../assets/Young Chang Images/images.jpg";
+import sonorLogo from "../assets/Sonor/images (1).png";
+import zildjianLogo from "../assets/zildjian/download.png";
+import palatinoLogo from "../assets/palatino/Logo+Black+with+HOME+Tag+2020.png";
+import mayBerlinLogo from "../assets/may berlin/MayBerlin-300x146.jpg";
+import kurzweilLogo from "../assets/kurzweil/images.png"
+
+export const brandsDatas = [
+    {
+        id: Math.random(),
+        name: "Stagg",
+        logo: staggLogo,
+        summery: "برند استگ یک برند بلژیکی ساخت ساز و لوازم جانبی ساز میباشد برای مطالب بیشتر کلیک کنید",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        images: []
+    },
+    {
+        id: Math.random(),
+        name: "Esteve",
+        logo: steveLogo,
+        summery: "برند اسنیو سازنده گیتار های دست ساز اسپانیایی میباشد برای مطالب بیشتر کلیک کنید",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        images: []
+    },
+    {
+        id: Math.random(),
+        name: "Steinway and Sons",
+        logo: steinwayLogo,
+        summery: "برند اشتنوی اند سانز معتبر ترین برند پیانو جهان میباشد که در تمامی کنسرت های بزرگ جهان مورد استفاده است",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        images:[]
+    },
+    {
+      id: Math.random(),
+      name: "Rotosound",
+      logo: rotosoundLogo,
+      summery: "سیم های روتوساند ساخت کشور انگلستان است و یکی از برترین سیم های جهان است برای مطالب بیشتر کلیک کنید",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      images: []
+    },
+    {
+      id: Math.random(),
+      name: "Young Chang",
+      logo: youngChangLogo,
+      summery: "پیانو های یانگ چانگ ساخت کشور کره میباشد ویکی از برترین برند های پیانو حال حاضر جهان است",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      images: []
+    },
+    {
+        id: Math.random(),
+        name: "Sonor",
+        logo: sonorLogo,
+        summery: "سونور یک برند آلمانی ساخت و تولید درامز و ساز های کوبه ای است که با کیفیت ترین ساز های کوبه ای است",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        images: []
+    },
+    {
+        id: Math.random(),
+        name: "Zildjian",
+        logo: zildjianLogo,
+        summery: "زیلجیان یکی از برنرین سازنده های سنج در جهان است این سنج را تمامی درام نوازان جدی جهان به خوبی میشناسند",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        images: []
+    },
+    {
+        id: Math.random(),
+        name: "palatino",
+        logo: palatinoLogo,
+        summery: "پالاتینو یک برند پیانو ایتالیایی است گ+که ساز هایی با کیفیت و اصلت اروپایی را تولید و عرضه مینماید",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        images: []
+    },
+    {
+        id: Math.random(),
+        name: "May Berlin",
+        logo: mayBerlinLogo,
+        summery: "می برلین یک برند ساخت تولید پیانو از زیر مجموعه های برند شیمل میباشد برای اطلاعات بیشتر کلیک کنید",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        images: []
+    },
+    {
+        id: Math.random(),
+        name: "Kurzweil",
+        logo: kurzweilLogo,
+        summery: "برند کورزویل یک برند انحصاری تولید پیانو دیچیتال آمریکایی است برای مشاهده بیشتر کلیک کنید",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        images: []
+    }
+  ]
