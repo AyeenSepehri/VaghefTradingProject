286d43088c3bd6c182ffae14b301dc470d3e978e
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-05-01 19:31:16 +0330

    create a wrapper component

---
+++ src/Layout/Wrapper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from "./Wrapper.module.css"
+
+function Wrapper(props) {
+  return (
+    <div className={classes.root}>{props.children}</div>
+  )
+}
+
+export default Wrapper
