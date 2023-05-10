7a0447fadcacddbde93613bb9e0373887030cef7
AyeenSepehri <sepehriayeen96prgr@gmail.com>
2023-05-10 20:19:13 +0330

    modify the name of tanglewood

--- src/store/arrayOfDatas.js
+++ src/store/arrayOfDatas.js
@@ -94,7 +94,7 @@ export const brandsDatas = [
     },
     {
         id: Math.random(),
-        name: "tanglewoodLogo",
+        name: "tanglewood",
         logo: tanglewoodLogo,
         summery: "تنگلوود یک برند تولید کننده گیتار است که در کشور انگلستان بسیار محبوب است برای مشاهده بیشتر کلیک کنید",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
